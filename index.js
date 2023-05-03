e0c79062065d2831c52a60a2d16219616af00e76
harun560 <harun560@yahoo.com>
2023-05-02 22:15:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('#left');
 cards.forEach(card => {
   const maximizeBtn = card.querySelector('.maximize-btn');
   maximizeBtn.addEventListener('click', () => {
